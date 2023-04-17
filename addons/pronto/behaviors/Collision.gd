ca1f227eb92cceb16579c5ad147e4ed383e764ae
Tom Beckmann <tomjonabc@gmail.com>
2023-04-17 20:00:41 +0200

    expand support of collision behavior

--- addons/pronto/behaviors/Collision.gd
+++ addons/pronto/behaviors/Collision.gd
@@ -9,8 +9,13 @@ signal collided(other: Area2D)
 func _ready():
 	super._ready()
 	if not Engine.is_editor_hint():
-		(get_parent() as Area2D).area_entered.connect(on_collision)
-		(get_parent() as Area2D).body_entered.connect(on_collision)
+		if get_parent() is Area2D:
+			(get_parent() as Area2D).area_entered.connect(on_collision)
+			(get_parent() as Area2D).body_entered.connect(on_collision)
+		if get_parent() is PhysicsBody2D:
+			(get_parent() as PhysicalBone2D).body_entered.connect(on_collision)
+		if get_parent() is StaticBody2D:
+			push_error("StaticBody2D cannot report collisions in Godot. Move the Collision to the other collision partner.")
 
 func on_collision(other: Node):
 	if limit_to_group == "" or other.is_in_group(limit_to_group):
--- addons/pronto/helpers/Connection.gd
+++ addons/pronto/helpers/Connection.gd
@@ -105,7 +105,7 @@ func _trigger(from: Object, argument_names: Array, argument_values: Array):
 	values.append(from)
 	if to:
 		var target = from.get_node(to)
-		names.append("target")
+		names.append("to")
 		values.append(target)
 		target.callv(invoke, arguments.map(func (arg): return ConnectionsList.eval(arg, names, values)))
 	else:
