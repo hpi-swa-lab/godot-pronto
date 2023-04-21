c218c31715a663b33fc21a87437f4a0551ecefc8
Tom Beckmann <tomjonabc@gmail.com>
2023-04-21 17:21:39 +0200

    display a warning when Collision is used on an invalid node

--- addons/pronto/behaviors/Collision.gd
+++ addons/pronto/behaviors/Collision.gd
@@ -12,8 +12,8 @@ func _ready():
 		if get_parent() is Area2D:
 			(get_parent() as Area2D).area_entered.connect(on_collision)
 			(get_parent() as Area2D).body_entered.connect(on_collision)
-		if get_parent() is PhysicsBody2D:
-			(get_parent() as PhysicalBone2D).body_entered.connect(on_collision)
+		if get_parent() is RigidBody2D:
+			(get_parent() as RigidBody2D).body_entered.connect(on_collision)
 		if get_parent() is StaticBody2D:
 			push_error("StaticBody2D cannot report collisions in Godot. Move the Collision to the other collision partner.")
 
@@ -21,5 +21,18 @@ func on_collision(other: Node):
 	if limit_to_group == "" or other.is_in_group(limit_to_group):
 		collided.emit(other)
 
+func is_valid_parent():
+	var p = get_parent()
+	return p is Area2D or p is RigidBody2D
+
 func lines():
-	return super.lines() + [Lines.Line.new(self, get_parent(), func (f): return "")]
+	return super.lines() + ([Lines.Line.new(self, get_parent(), func (f): return "")] if is_valid_parent() else [])
+
+func _notification(what):
+	if what == NOTIFICATION_PARENTED:
+		update_configuration_warnings()
+
+func _get_configuration_warnings():
+	if not is_valid_parent():
+		return ["Collision only works with Area2D and RigidBody2D"]
+	return ""
