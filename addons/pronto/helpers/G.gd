931d7758168054345f8bc6f72701defb5807e9c9
Tom Beckmann <tomjonabc@gmail.com>
2023-04-22 18:39:21 +0200

    update state changes to global in local states

--- addons/pronto/helpers/G.gd
+++ addons/pronto/helpers/G.gd
@@ -6,12 +6,22 @@ extends Node
 ### Access via G.at("prop").
 
 var values = {}
+var _state_update = {}
 
 func put(name: String, value: Variant):
-	values[name] = value
+	_put(name, value)
+	if name in _state_update:
+		_state_update[name].put(name, value)
 
 func at(name: String, default = null):
 	if name in values:
 		return values[name]
 	else:
 		return default
+
+func _put(name: String, value: Variant):
+	values[name] = value
+
+func _register_state(state: State, prop: String):
+	assert(not prop in _state_update, "Property {0} has already been registered by a state node.".format([prop]))
+	_state_update[prop] = state
