a04f5780b2a86372e1245f4f81741b010ba8dfc0
Tom Beckmann <tomjonabc@gmail.com>
2023-04-23 17:52:44 +0200

    for bound properties, show button instead

--- addons/pronto/helpers/ExpressionInspector.gd
+++ addons/pronto/helpers/ExpressionInspector.gd
@@ -18,11 +18,27 @@ class ExpressionProperty extends EditorProperty:
 			editor.text = val
 			my_update = false
 
+class BoundProperty extends EditorProperty:
+	func _init(bind: Bind):
+		var l = Button.new()
+		l.text = "Bound. Click to see Bind."
+		l.pressed.connect(func ():
+			var s = G.at("_pronto_editor_plugin").get_editor_interface().get_selection()
+			s.clear()
+			s.add_node(bind))
+		add_child(l)
+	func _update_property():
+		pass
+
 func _can_handle(object):
-	return object is Bind
+	return (object is Bind
+		or object is Node and object.get_child_count() > 0 and object.get_child(0) is Bind)
 
 func _parse_property(object, type, name, hint_type, hint_string, usage_flags, wide):
-	if name == "convert":
+	if object is Bind and name == "convert":
 		add_property_editor(name, ExpressionProperty.new())
+		return true
+	if object is Node and object.get_child_count() > 0 and object.get_child(0) is Bind and object.get_child(0).to_prop == name:
+		add_property_editor(name, BoundProperty.new(object.get_child(0)))
 		return true
 	return false
