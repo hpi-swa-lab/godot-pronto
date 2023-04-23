03609da49b8fcac943de32eabbabef88086114a9
Tom Beckmann <tomjonabc@gmail.com>
2023-04-23 10:24:28 +0200

    slightly better syntax highlighting

--- addons/pronto/signal_connecting/expression_edit.gd
+++ addons/pronto/signal_connecting/expression_edit.gd
@@ -20,10 +20,71 @@ signal text_changed()
 
 func _ready():
 	$OpenFile.icon = Utils.icon_from_theme("DebugSkipBreakpointsOff", self)
-	# causes crashes
-	# get_a_godot_highlighter()
+	fake_a_godot_highlighter()
+
+func fake_a_godot_highlighter():
+	var s = G.at("_pronto_editor_plugin").get_editor_interface().get_editor_settings()
+	var h = CodeHighlighter.new()
+	h.number_color = s.get("text_editor/theme/highlighting/number_color")
+	h.symbol_color = s.get("text_editor/theme/highlighting/symbol_color")
+	h.function_color = s.get("text_editor/theme/highlighting/function_color")
+	h.member_variable_color = s.get("text_editor/theme/highlighting/member_variable_color")
+	
+	var control_flow_keyword_color = s.get("text_editor/theme/highlighting/keyword_color")
+	var keyword_color = s.get("text_editor/theme/highlighting/keyword_color")
+	h.keyword_colors = {
+		"break": control_flow_keyword_color,
+		"continue": control_flow_keyword_color,
+		"elif": control_flow_keyword_color,
+		"else": control_flow_keyword_color,
+		"if": control_flow_keyword_color,
+		"for": control_flow_keyword_color,
+		"match": control_flow_keyword_color,
+		"pass": control_flow_keyword_color,
+		"return": control_flow_keyword_color,
+		"while": control_flow_keyword_color,
+		# operators
+		"and": keyword_color,
+		"in": keyword_color,
+		"not": keyword_color,
+		"or": keyword_color,
+		# types and values
+		"false": keyword_color,
+		"float": keyword_color,
+		"int": keyword_color,
+		"bool": keyword_color,
+		"null": keyword_color,
+		"PI": keyword_color,
+		"TAU": keyword_color,
+		"INF": keyword_color,
+		"NAN": keyword_color,
+		"self": keyword_color,
+		"true": keyword_color,
+		"void": keyword_color,
+		# functions
+		"as": keyword_color,
+		"assert": keyword_color,
+		"await": keyword_color,
+		"breakpoint": keyword_color,
+		"class": keyword_color,
+		"class_name": keyword_color,
+		"extends": keyword_color,
+		"is": keyword_color,
+		"func": keyword_color,
+		"preload": keyword_color,
+		"signal": keyword_color,
+		"super": keyword_color,
+		# var
+		"const": keyword_color,
+		"enum": keyword_color,
+		"static": keyword_color,
+		"var": keyword_color,
+	}
+	$Expression.syntax_highlighter = h
 
 func get_a_godot_highlighter():
+	# NOTE: causes crashes when saving scenes in the current form. Potentially because
+	# we are sharing the instance between multiple editors  (which is not technically allowed)
 	# GDScriptHighlighter is a private cpp class. We can obtain an instance however.
 	var se = G.at("_pronto_editor_plugin").get_editor_interface().get_script_editor()
 	if not se.get_open_script_editors().is_empty():
