734fe5357f9ad1999edd5d9eb80a68c8223849fe
Finn-HPI <finn.schoellkopf@student.hpi.uni-potsdam.de>
2023-06-02 12:00:35 +0200

    add snake blocks

--- prototypes/MovingPlatform.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@onready var _follow :PathFollow2D = get_parent()
-@export var _speed :float = 120.0
-
-func _physics_process(delta):
-	_follow.set_progress(_follow.get_progress() + _speed * delta)
---
+++ prototypes/i3w1-mario-moving-snake/MovingPlatform.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var _follow: PathFollow2D = get_parent()
+@export var speed: float = 60.0
+var is_finished: bool = false
+
+func _physics_process(delta):
+	if is_finished:
+		get_node("Placeholder").color = Color.DARK_GREEN
+		return
+	_follow.set_progress(_follow.get_progress() + speed * delta)
+	if not _follow.loop && _follow.get_progress_ratio() >= 1:
+		is_finished = true
+		for follow in _follow.get_parent().get_children():
+			follow.get_node("SnakeBlock").is_finished = true
+			follow.get_node("Clock").reset_and_start()
