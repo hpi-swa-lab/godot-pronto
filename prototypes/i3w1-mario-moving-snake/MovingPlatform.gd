78a6c1dd8594b841e384414b3956ca9099b494b1
JulianEgbert <Julian.Egbert@student.hpi.de>
2023-06-04 16:15:44 +0200

    Add missed files of last commit

--- prototypes/i3w1-mario-moving-snake/MovingPlatform.gd
+++ prototypes/i3w1-mario-moving-snake/MovingPlatform.gd
@@ -2,9 +2,12 @@ extends CharacterBody2D
 
 @onready var _follow: PathFollow2D = get_parent()
 @export var speed: float = 60.0
+@export var paused: bool = true
 var is_finished: bool = false
 
 func _physics_process(delta):
+	if (paused):
+		return
 	if is_finished:
 		get_node("Placeholder").color = Color.DARK_GREEN
 		return
@@ -14,3 +17,9 @@ func _physics_process(delta):
 		for follow in _follow.get_parent().get_children():
 			follow.get_node("SnakeBlock").is_finished = true
 			follow.get_node("Clock").reset_and_start()
+			
+func activate_snake():
+	if not paused:
+		return
+	for follow in _follow.get_parent().get_children():
+		follow.get_node("SnakeBlock").paused = false
