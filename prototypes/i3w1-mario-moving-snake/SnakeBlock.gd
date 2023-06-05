aed925ca3f223712c919bef672cfa657b3983779
Finn Schoellkopf <schoellkopf.finn@gmail.com>
2023-06-05 15:01:40 +0200

    add value panel and start working on falling platforms

--- prototypes/i3w1-mario-moving-snake/SnakeBlock.gd
+++ prototypes/i3w1-mario-moving-snake/SnakeBlock.gd
@@ -1,7 +1,6 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 @onready var _follow: PathFollow2D = get_parent()
-@export var speed: float = 60.0
 @export var paused: bool = true
 var is_finished: bool = false
 
@@ -11,12 +10,15 @@ func _physics_process(delta):
 	if is_finished:
 		get_node("Placeholder").color = Color.DARK_GREEN
 		return
+	var speed = G.at("SnakeSpeed")
 	_follow.set_progress(_follow.get_progress() + speed * delta)
-	if not _follow.loop && _follow.get_progress_ratio() >= 1:
+	if _follow.get_progress_ratio() >= 1:
 		is_finished = true
 		for follow in _follow.get_parent().get_children():
 			follow.get_node("SnakeBlock").is_finished = true
-			follow.get_node("Clock").reset_and_start()
+			var clock = follow.get_node("Clock")
+			clock.duration_seconds = G.at("SnakeDespawnTime")
+			clock.reset_and_start()
 			
 func activate_snake():
 	if not paused:
