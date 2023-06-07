7811c6ff7a3af31d51252ef5ff3da49c93c53be4
Jan Reppien <jan.reppien@uni-bielefeld.de>
2023-06-07 16:10:29 +0200

    Add moveable platforms as well as paltform with different collision masks plus demo level

---
+++ prototypes/i3w1-platform-control/Path2D.gd
@@ -0,0 +1,15 @@
+extends Path2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _draw():
+	draw_polyline(self.curve.get_baked_points(), Color.AQUAMARINE, 5, true)
