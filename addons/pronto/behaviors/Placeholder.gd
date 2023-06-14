4f2ea8623e2c0718aad1e01722d8bdca280ecc0c
LucPrestin <l.prestin@gmx.de>
2023-06-14 11:19:23 +0200

    apply transform to handle

--- addons/pronto/behaviors/Placeholder.gd
+++ addons/pronto/behaviors/Placeholder.gd
@@ -77,11 +77,19 @@ func _draw():
 		debug_color.a = 1
 		draw_rect(r, debug_color, false)
 
+# position: Vector2, 
+# icon: Texture,
+# object: Object, 
+# property: String, 
+# map: Callable, 
+# local_space = true
+
 func handles():
 	return [
-		Handles.SetPropHandle.new(placeholder_size / 2,
+		Handles.SetPropHandle.new(
+			transform * placeholder_size / 2,
 			Utils.icon_from_theme("EditorHandle", self),
 			self,
 			"placeholder_size",
-			func (coord): return floor(coord * 2).clamp(Vector2(1, 1), Vector2(10000, 10000)))
+			func (coord): return (floor(coord * 2) * transform).clamp(Vector2(1, 1), Vector2(10000, 10000)))
 	]
