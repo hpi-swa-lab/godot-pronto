a02d2917deda9fc360c7e057c2410575886fe077
Jan Reppien <jan.reppien@uni-bielefeld.de>
2023-06-15 14:33:39 +0200

    Remove AdvancedValue, Add move functionality of AdvancedValue to Value, Change icon of reset buttons in PrototypingUI

--- addons/pronto/behaviors/PrototypingUI.gd
+++ addons/pronto/behaviors/PrototypingUI.gd
@@ -20,14 +20,10 @@ func _ready():
 	vbox.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
 	
 	for childNode in self.get_children():
-		
-		# making sure to only create sliders for value behaviours
-		if is_instance_of(childNode, Value):
-			vbox.add_child(create_ui_slider_for_values(childNode))
-		elif is_instance_of(childNode, Code):
+		if is_instance_of(childNode, Code):
 			vbox.add_child(create_ui_for_code(childNode))
-		elif is_instance_of(childNode,AdvancedValue):
-			vbox.add_child(create_ui_for_advanced_value(childNode))
+		elif is_instance_of(childNode,Value):
+			vbox.add_child(create_ui_for_value(childNode))
 	
 	scrollContainer.add_child(vbox)
 	# adding through deferred call as it might fail otherwise
@@ -35,18 +31,18 @@ func _ready():
 	# print("Prototype UI generated successfully")
 	
 	
-func create_ui_for_advanced_value(advanced_value: AdvancedValue):
-	if advanced_value.selectType == "Float":
-		return create_ui_slider_for_advanced_value_float(advanced_value)
-	elif advanced_value.selectType == "Enum":
-		return create_ui_for_advanced_value_enum(advanced_value)
-	elif advanced_value.selectType == "Bool":
-		return create_ui_for_advanced_value_bool(advanced_value)
+func create_ui_for_value(value: Value):
+	if value.selectType == "Float":
+		return create_ui_slider_for_value_float(value)
+	elif value.selectType == "Enum":
+		return create_ui_for_value_enum(value)
+	elif value.selectType == "Bool":
+		return create_ui_for_value_bool(value)
 	return
 	
 	
 	
-func create_ui_for_advanced_value_enum(value: AdvancedValue):
+func create_ui_for_value_enum(value: Value):
 	var name = value.name
 	var label = Label.new()
 	label.text = name + ":"
@@ -62,12 +58,12 @@ func create_ui_for_advanced_value_enum(value: AdvancedValue):
 		index = index + 1
 
 	optionButton.select(value.enum_default_index)
-	optionButton.item_selected.connect(handle_advanced_value_enum_change.bind(value,optionButton))
+	optionButton.item_selected.connect(handle_value_enum_change.bind(value,optionButton))
 	
 	var reset_button = Button.new()
-	reset_button.text = "R"
+	reset_button.text = "⟳"
 	reset_button.focus_mode = 0
-	reset_button.button_down.connect(handle_advanced_value_enum_change.bind(value.enum_default_index, value, optionButton))
+	reset_button.button_down.connect(handle_value_enum_change.bind(value.enum_default_index, value, optionButton))
 	
 	hbox.add_child(label)
 	hbox.add_child(optionButton)
@@ -76,7 +72,7 @@ func create_ui_for_advanced_value_enum(value: AdvancedValue):
 	
 	return hbox
 	
-func create_ui_for_advanced_value_bool(value: AdvancedValue):
+func create_ui_for_value_bool(value: Value):
 	
 	var name = value.name
 	var label = Label.new()
@@ -98,18 +94,18 @@ func create_ui_for_advanced_value_bool(value: AdvancedValue):
 	else:
 		optionButton.select(0)
 	
-	#hslider.value_changed.connect(handle_slider_update_advanced_value_float_change.bind(value))
-	optionButton.item_selected.connect(handle_advanced_value_bool_change.bind(value, optionButton))
+	#hslider.value_changed.connect(handle_update_value_float_change.bind(value))
+	optionButton.item_selected.connect(handle_value_bool_change.bind(value, optionButton))
 	
 	var reset_button = Button.new()
-	reset_button.text = "R"
+	reset_button.text = "⟳"
 	reset_button.focus_mode = 0
 	var reset_index: int
 	if value.bool_value == "TRUE":
 		reset_index = 1
 	else:
 		reset_index = 0
-	reset_button.button_down.connect(handle_advanced_value_bool_change.bind(reset_index, value, optionButton))
+	reset_button.button_down.connect(handle_value_bool_change.bind(reset_index, value, optionButton))
 	
 	hbox.add_child(label)
 	hbox.add_child(optionButton)
@@ -124,7 +120,7 @@ func create_ui_for_code(code: Code):
 	return(button)
 	
 	
-func create_ui_slider_for_advanced_value_float(value: AdvancedValue):
+func create_ui_slider_for_value_float(value: Value):
 	var name = value.name
 	#print("Adding slider for " + name)
 	
@@ -182,15 +178,15 @@ func create_ui_slider_for_advanced_value_float(value: AdvancedValue):
 	# setting focus mode to ignore keyboard input
 	hslider.focus_mode = Slider.FOCUS_NONE
 	# signal | this potentially fires very often during dragging of the slider
-	hslider.value_changed.connect(handle_slider_update_advanced_value_float_change.bind(value,label_current, hslider))
+	hslider.value_changed.connect(handle_update_value_float_change.bind(value,label_current, hslider))
 	# fancy ui signals
 	hslider.drag_started.connect(_handle_slider_drag_start.bind(lower_hbox))
 	hslider.drag_ended.connect(_handle_slider_drag_end.bind(lower_hbox))
 	
 	var reset_button = Button.new()
-	reset_button.text = "R"
+	reset_button.text = "⟳"
 	reset_button.focus_mode = 0
-	reset_button.button_down.connect(handle_slider_update_advanced_value_float_change.bind(value.float_default,value,label_current,hslider))
+	reset_button.button_down.connect(handle_update_value_float_change.bind(value.float_default,value,label_current,hslider))
 	
 	middle_vbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	middle_vbox.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
@@ -206,97 +202,18 @@ func create_ui_slider_for_advanced_value_float(value: AdvancedValue):
 	return hbox
 
 
-#TODO: Remove if Advanced Value replaces Value
-func create_ui_slider_for_values(value: Value):
-	var name = value.name
-	#print("Adding slider for " + name)
-	
-	var hbox = HBoxContainer.new()
-	var middle_vbox = VBoxContainer.new()
-	var lower_hbox = HBoxContainer.new()
-	var name_vbox = VBoxContainer.new()		
-	name_vbox.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
-
-
-	# value name label
-	var label = Label.new()
-	label.text = name + ":"
-	label.vertical_alignment = VERTICAL_ALIGNMENT_TOP
-	name_vbox.add_child(label)
-	
-	# value min label
-	var label_min = Label.new()
-	label_min.text = str(value.from)
-	label_min.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	label_min.add_theme_color_override("font_color",muted_gray)
-	
-	# value max label
-	var label_max = Label.new()
-	label_max.text = str(value.to)
-	label_max.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	label_max.add_theme_color_override("font_color",muted_gray)
-	
-	var current_vbox = VBoxContainer.new()
-	current_vbox.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
-	
-	# current value label
-	var label_current = Label.new()
-	label_current.text = str(value)
-	label_current.vertical_alignment = VERTICAL_ALIGNMENT_TOP
-	current_vbox.add_child(label_current)
-
-	lower_hbox.add_child(label_min)
-	lower_hbox.add_spacer(false)
-	lower_hbox.add_child(label_max)
-	lower_hbox.visible = false
-	
-	# slider 
-	var hslider = HSlider.new()
-	hslider.min_value = value.from
-	hslider.max_value = value.to
-	hslider.step = value.step_size
-	hslider.value = value.value
-	# setting growth flags so that the slider resized to parent container
-	hslider.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	hslider.size_flags_vertical = Control.SIZE_SHRINK_CENTER
-
-	# setting focus mode to ignore keyboard input
-	hslider.focus_mode = Slider.FOCUS_NONE
-	# signal | this potentially fires very often during dragging of the slider
-	hslider.value_changed.connect(handle_slider_update.bind(name, label_current))
-	# fancy ui signals
-	hslider.drag_started.connect(_handle_slider_drag_start.bind(lower_hbox))
-	hslider.drag_ended.connect(_handle_slider_drag_end.bind(lower_hbox))
-	
-	middle_vbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	middle_vbox.size_flags_vertical = Control.SIZE_SHRINK_BEGIN
-	middle_vbox.add_child(hslider)
-	middle_vbox.add_child(lower_hbox)
-	
-	# adding to node tree
-	hbox.add_child(name_vbox)
-	hbox.add_child(middle_vbox)
-	hbox.add_child(current_vbox)
-	return hbox
-	
-	
-	
-func handle_advanced_value_bool_change(index: int, value: AdvancedValue, optionButton : OptionButton):
+func handle_value_bool_change(index: int, value: Value, optionButton : OptionButton):
 	optionButton.select(index)
 	value.bool_value = optionButton.get_item_text(index)
 	
-func handle_advanced_value_enum_change(index: int, value: AdvancedValue, optionButton: OptionButton):
+func handle_value_enum_change(index: int, value: Value, optionButton: OptionButton):
 	optionButton.select(index)
 	value.enum_value = value.enum_choices[index]
 	
-func handle_slider_update_advanced_value_float_change(new_value: float, value: AdvancedValue, label_current: Label, slider :HSlider):
+func handle_update_value_float_change(new_value: float, value: Value, label_current: Label, slider :HSlider):
 	value.float_value = new_value
 	slider.value = new_value
 	label_current.text = str(new_value)
-	
-func handle_slider_update(value: float, varRef: String, label_current: Label):
-	label_current.text = str(value)
-	#G.put(varRef, value)
 	
 func _handle_slider_drag_start(hbox: HBoxContainer):
 	hbox.visible = true
@@ -314,7 +231,7 @@ func _get_configuration_warnings():
 
 func _at_least_one_valid_child():
 	for childNode in self.get_children():
-		if is_instance_of(childNode, Value) or is_instance_of(childNode, AdvancedValue):
+		if is_instance_of(childNode, Code) or is_instance_of(childNode, Value):
 			return true
 	return false
 	
