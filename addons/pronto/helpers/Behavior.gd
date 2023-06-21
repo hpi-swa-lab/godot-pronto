hpi-swa-lab/godot-pronto

commit 4d79632188a56f6d67e14f7a818afff486f6ae4b

use green for highlighting connections and red for activating connections

addons/pronto/helpers/Behavior.gd
@@ -66,15 +66,17 @@ func connection_activated(c: Connection):
 			current.kill()
 	var t = create_tween()
 	_running_tweens[c] = t
-	if current == null:
-		t.tween_method(flash_line.bind(c), 0.0, 1.0, 0.2)
-	t.tween_method(flash_line.bind(c), 1.0, 0.0, 0.2)
+	t.tween_method(
+		flash_line.bind(c, 'activated'),
+		0.0 if current == null else 1.0,
+		1.0 if current == null else 0.0,
+		0.2
+	)
 	
 # duplicating as Godot does not support overloading
 # see: https://github.com/godotengine/godot-proposals/issues/1571
 var _running_highlight_tweens = {}
 func highlight_activated(c: Connection):
-	# FOR LATER: use different color for highlight and activation
 	var duration = 0.3
 	# FIXME not scheduling well yet on fast repeats
 	var current = _running_highlight_tweens.get(c)
@@ -83,10 +85,10 @@ func highlight_activated(c: Connection):
 		current.kill()
 	var t = create_tween()
 	_running_highlight_tweens[c] = t
-	t.tween_method(flash_line.bind(c), 1.0, 0.0, duration)
+	t.tween_method(flash_line.bind(c, 'highlight'), 1.0, 0.0, duration)
 
-func flash_line(value: float, key: Variant):
-	_lines.flash_line(value, key)
+func flash_line(value: float, key: Variant, type: String):
+	_lines.flash_line(key, type, value)
 	queue_redraw()
 
 func lines() -> Array:

addons/pronto/helpers/Lines.gd
@@ -8,8 +8,13 @@ var _flashed = {}
 var _enabled = {}
 
 
-func flash_line(value: float, key: Variant):
-	_flashed[key] = value
+func flash_line(key: Variant, type: String, value: float):
+	_flashed[[key, type]] = value
+
+const flash_colors = {
+	'activated': Color.RED,
+	'highlight': Color.GREEN,
+}
 
 
 func set_enabled(value: bool, key: Variant):
@@ -66,8 +71,13 @@ class Line:
 	
 	func color(lines: Lines):
 		if not lines._enabled.get(key,true):
-			return Color.WHITE.lerp(Color.BLACK, 0.5);
-		return Color.WHITE.lerp(Color.RED, lines._flashed.get(key, 0.0))
+			return Color.WHITE.lerp(Color.BLACK, 0.5)
+		var color = Color.BLACK
+		for type in flash_colors:
+			var flash = lines._flashed.get([key, type], 0.0)
+			if flash > 0.0:
+				color += Color.WHITE.lerp(flash_colors[type], flash)
+		return color if color != Color.BLACK else Color.WHITE
 	
 	func draw_text(c: CanvasItem, font: Font, text_size: int, flipped: bool, lines: Lines):
 		var text = text_fn.call(flipped)

commit 24ab6f1ca1ed44c3d77a43e7a3ce38d589545bd4

fix connection highlights not disappearing
regression from 4d79632188a56f6d67e14f7a818afff486f6ae4b

addons/pronto/helpers/Behavior.gd
@@ -68,12 +68,9 @@ func connection_activated(c: Connection):
 			current.kill()
 	var t = create_tween()
 	_running_tweens[c] = t
-	t.tween_method(
-		flash_line.bind(c, 'activated'),
-		0.0 if current == null else 1.0,
-		1.0 if current == null else 0.0,
-		0.2
-	)
+	if current == null:
+		t.tween_method(flash_line.bind(c, 'activated'), 0.0, 1.0, 0.2)
+	t.tween_method(flash_line.bind(c, 'activated'), 1.0, 0.0, 0.2)
 	
 # duplicating as Godot does not support overloading
 # see: https://github.com/godotengine/godot-proposals/issues/1571