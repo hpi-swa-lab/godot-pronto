2423173ca4bf4d405d0ea65c6ed7e63ab668b4be
Christoph Thiede <christoph.thiede@student.hpi.de>
2023-06-28 15:02:52 +0200

    synchronize resize handles; fix their size

--- addons/pronto/helpers/ResizableContainer.gd
+++ addons/pronto/helpers/ResizableContainer.gd
@@ -5,6 +5,11 @@ class_name ResizableContainer
 # Add a single child node to this node. The child node will be displayed with a handle that the user can drag to resize the child node.
 # The child may customize handling of the resize by providing a `dragged_minimum_size` property and a `resize()` method.
 
+# All resize handles within a specified parent can be synchronized.
+@export var sync_handles_parent_path: NodePath = ^""
+@export var sync_handles_x = false
+@export var sync_handles_y = false
+
 var child:
 	get:
 		# TODO: should we make the handle internal?
@@ -13,6 +18,9 @@ var child:
 var _drag_start_offset = null
 
 func _process(delta):
+	# force layout of handle (workaround because it somehow always resizes with the parent)
+	%Handle.set_size(Vector2(16, 16))
+	
 	if self.child == null:
 		return
 	# update handle position
@@ -33,7 +41,7 @@ func _gui_input(event):
 		_drag(event.global_position)
 
 func _start_drag(_position: Vector2):
-	_drag_start_offset = _position - self.child.get_size()
+	_drag_start_offset = _position - get_child_size()
 
 func _stop_drag():
 	_drag_start_offset = null
@@ -41,8 +49,27 @@ func _stop_drag():
 func _drag(_position: Vector2):
 	if self.child == null:
 		return
+	set_child_size(_position - _drag_start_offset)
+	
+	if self.sync_handles_parent_path != ^"":
+		var parent = get_node(self.sync_handles_parent_path)
+		print("sync parent", parent)
+		var otherContainers = Utils.all_nodes_that(parent, func(node): return node is ResizableContainer and node != self)
+		for otherContainer in otherContainers:
+			print("sync", otherContainer)
+			var other_child_size = otherContainer.get_child_size()
+			if self.sync_handles_x:
+				other_child_size.x = get_child_size().x
+			if self.sync_handles_y:
+				other_child_size.y = get_child_size().y
+			otherContainer.set_child_size(other_child_size)
+
+func get_child_size():
+	return self.child.get_size()
+
+func set_child_size(size: Vector2):
 	if 'dragged_minimum_size' in self.child:
-		self.child.dragged_minimum_size = _position - _drag_start_offset
+		self.child.dragged_minimum_size = size
 		self.child.resize()
 	else:
-		self.child.set_size(_position - _drag_start_offset)
+		self.child.set_size(size)
