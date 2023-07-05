b99103e7866eeb578c3d721d6627d6329f3e32e5
Christoph Thiede <christoph.thiede@student.hpi.de>
2023-07-05 10:51:03 +0200

    spawn adversary pets, get collisisions between ours and their to work, add flash + camerashake, add battle mode, add second health bar and connect them to values, add second pet
    next: actually update healthbars and add reactions.

--- prototypes/i5w1-sap/pet.gd
+++ prototypes/i5w1-sap/pet.gd
@@ -1,20 +1,21 @@
 extends RigidBody2D
 
-
 @export var petname := ""
 
+var default_health:
+	get:
+		return G.at("%s_health" % petname)
+
+var attacks:
+	get:
+		return G.at("%s_attacks" % petname)
 
 var _queued_position 
 
-
 func queue_position(pos: Vector2):
 	_queued_position = pos
-
 
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	if _queued_position != null:
 		state.transform.origin = _queued_position
 		_queued_position = null
-
-func get_damage() -> int:
-	return G.at("%s_damage" % petname)
