49232a6fea77896a49ad2701a635af5b945d8596
JulianEgbert <Julian.Egbert@student.hpi.de>
2023-07-09 13:06:49 +0200

    Update Code-Behavior

--- addons/pronto/behaviors/Code.gd
+++ addons/pronto/behaviors/Code.gd
@@ -3,12 +3,15 @@
 extends Behavior
 class_name Code
 
+## Emittet when the execution of the code returns. Contains the result of the execution as parameter.
 signal after(result)
 
-@export var label: String = ""
+## The names of the arguments that the code receives. Those can be accessed in [code]evaluate[/code].
 @export var arguments: PackedStringArray:
 	set(a): if evaluate: evaluate.argument_names = a
 	get: return PackedStringArray(evaluate.argument_names) if evaluate else []
+
+## The Code that gets executed when [code]execute()[/code] method is called.
 @export var evaluate: ConnectionScript
 
 func _ready():
@@ -17,9 +20,11 @@ func _ready():
 		evaluate = ConnectionScript.new()
 		evaluate.argument_names = arguments
 
+# Draws the name of the node (from the SceneTree) below the node
 func lines():
-	return super.lines() + [Lines.BottomText.new(self, label)]
+	return super.lines() + [Lines.BottomText.new(self, self.name)]
 
+## Executes the code provided in [code]evaluate[/code] and emits the [code]after[/code] signal afterwards.
 func execute(args: Array):
 	assert(args.size() == arguments.size(), "Argument names and values for eval need to have the same size.")
 	var result = evaluate.run(args, self)
---
+++ changelog/i5w2_jf_changelog.md
@@ -0,0 +1,7 @@
+# Iteration 5 Week 2 - Julian & Finn
+
+## Code-Behavior
+
+We removed the label from the Code-Behavior and use its name from the SceneTree instead. We found ourself labeling nodes twice (once in the tree and then again in the label) so we decided to change this.
+
+Also we added comments to the Behavior.
