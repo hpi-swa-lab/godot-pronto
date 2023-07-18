e4617de94828ffc8786f8169054b1ff2411673a7
JohnAEX <grenda.jonathan@gmail.com>
2023-07-18 20:19:49 +0200

    Further work

---
+++ prototypes/i6w1-fishing/custom_cam.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@export var target_to_follow: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y = target_to_follow.position.y
---
+++ prototypes/i6w1-fishing/start_button.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(_start)
+	
+	
+func _start():
+	G.get_parent().find_child("menu", true, false).exit("")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
