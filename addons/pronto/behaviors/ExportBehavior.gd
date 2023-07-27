8ba3dff52fcfbc91f492c63ac4ddb7ac04a9b054
JulianEgbert <Julian.Egbert@student.hpi.de>
2023-07-27 22:50:33 +0200

    ExportBehavior documentation and minor changes

--- addons/pronto/behaviors/ExportBehavior.gd
+++ addons/pronto/behaviors/ExportBehavior.gd
@@ -3,44 +3,53 @@
 extends Behavior
 class_name ExportBehavior
 
-@export var enable: bool = true
-@export_category("Game Info")
+## The display title of the game
 @export var title: String = "<Title>"
+
+## All authors of the game
 @export var authors: PackedStringArray = ["<Author>"]
+
+## The description of the game
 @export_multiline var description = ""
 
-@export_category("Export")
+## Settings for the Thumbnail
+@export_category("Thumbnail")
+
+## If true, a screenshot will be taken after a delay and saved as thumbnail.png
 @export var take_screenshot: bool = true
+
+## The delay until the screenshot is taken
 @export var wait_seconds: int = 1
 
 var timer: Timer
+var export_path
 
 func _ready():
 	super._ready()
-	if not enable: return;
-	# start export timer
+
+	var scene_path = get_tree().current_scene.scene_file_path
+	var tmp = scene_path.split("/")
+	var scene_name = tmp[tmp.size()-1];
+	export_path = scene_path.rstrip(scene_name)
+
+	_create_game_json()
+
+	# start thumbnail timer
 	timer = Timer.new()
-	timer.connect("timeout", _export) 
+	timer.connect("timeout", _take_screenshot) 
 	add_child(timer)
 	timer.one_shot = true
 	timer.wait_time = max(1, wait_seconds) # wait at least 1s
 	timer.start()
-
-func _export():
-	var scene_path = get_tree().current_scene.scene_file_path
-	var tmp = scene_path.split("/")
-	var scene_name = tmp[tmp.size()-1];
-	var export_path = scene_path.rstrip(scene_name)
-	_take_screenshot(export_path)
-	_create_game_json(export_path)
 	
-func _take_screenshot(export_path):
+func _take_screenshot():
+	if not take_screenshot: return
 	var vpt = get_viewport()
 	var tex = vpt.get_texture()
 	tex.get_image().save_png(export_path + "thumbnail.png")
 	print("screenshot taken")
 	
-func _create_game_json(export_path):
+func _create_game_json():
 	var game_dict = {
 		"title": title
 	}
@@ -48,12 +57,9 @@ func _create_game_json(export_path):
 		game_dict["authors"] = authors
 	if not description.is_empty():
 		game_dict["description"] = description
-	if take_screenshot:
-		game_dict["thumbnailType"] = "png"
 	
 	var path = export_path + "game_info.json"
 	var file = FileAccess.open(path, FileAccess.WRITE)
 
 	file.store_string(JSON.stringify(game_dict, "\t"))
 	file = null
-	print("export finished")
