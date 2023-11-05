184eedcd35588dfa5084d0bbcf68bd13b52a07dd
baerlach-git <83013270+baerlach-git@users.noreply.github.com>
2023-11-05 16:36:21 +0100

    final version

---
+++ prototypes/game-baerlachTest/blocker.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+func block():
+	collision_layer = 1
+	collision_mask = 1
+	visible = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ prototypes/game-baerlachTest/door1.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+func open(key):
+	if key == 1:
+		queue_free()
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
