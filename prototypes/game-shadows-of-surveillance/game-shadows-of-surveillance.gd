c6668b2a659c467e12d809377fa14c01bb5d09cc
ole1711 <42774821+ole1711@users.noreply.github.com>
2024-01-20 20:23:07 +0100

    set up basic split screen in godot 4

---
+++ prototypes/game-shadows-of-surveillance/game-shadows-of-surveillance.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var players := {
+	"1": { 
+		viewport = $LeftSubViewportContainer/SubViewport, 
+		camera = $LeftSubViewportContainer/SubViewport/Camera2D, 
+		player = $LeftSubViewportContainer/SubViewport/Level/Player1,
+	},
+	"2": {
+		viewport = $RightSubViewportContainer/SubViewport, 
+		camera = $RightSubViewportContainer/SubViewport/Camera2D, 
+		player = $LeftSubViewportContainer/SubViewport/Level/Player2,
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+
+	for node in players.values():
+		var remote_transform := RemoteTransform2D.new()
+		remote_transform.remote_path = node.camera.get_path()
+		node.player.add_child(remote_transform)
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.	
+func _process(delta):
+	pass
