5cd951dbc16028272c061fbc578874d854c25144
ole1711 <42774821+ole1711@users.noreply.github.com>
2024-01-21 16:36:15 +0100

    add 2 world 2 player split screen

--- prototypes/game-shadows-of-surveillance/game-shadows-of-surveillance.gd
+++ prototypes/game-shadows-of-surveillance/game-shadows-of-surveillance.gd
@@ -9,17 +9,17 @@ extends Node2D
 	"2": {
 		viewport = $RightSubViewportContainer/SubViewport, 
 		camera = $RightSubViewportContainer/SubViewport/Camera2D, 
-		player = $LeftSubViewportContainer/SubViewport/Level/Player2,
+		player = $RightSubViewportContainer/SubViewport/Level2/Player2,
 	}
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	players["2"].viewport.world_2d = players["1"].viewport.world_2d
 
 	for node in players.values():
 		var remote_transform := RemoteTransform2D.new()
 		remote_transform.remote_path = node.camera.get_path()
+		print(node)
 		node.player.add_child(remote_transform)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.	
