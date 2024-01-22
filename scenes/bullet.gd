b16fbd6dce61f481ee2e493a11505dee1462bda8
David Schroschk <mustermannmuster2@gmail.com>
2024-01-22 16:21:16 +0100

    Pew pew mit dem lieben @ole1711

---
+++ scenes/bullet.gd
@@ -0,0 +1,24 @@
+extends Area2D
+var speed = 501
+var dir
+var muzzle
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.global_position = muzzle
+	global_rotation = dir.angle()
+
+func setup(direction, muzzlePos):
+	dir = direction
+	muzzle = muzzlePos
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += dir * speed * delta
+	print(position)
+
+func _on_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.queue_free()
+	if !body.is_in_group("player"):
+		queue_free()
---
+++ scenes/player2.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+const bullet_scene = preload("res://scenes/bullet.tscn")
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+  
+	if Input.is_action_just_pressed("shoot"): 
+		print("pew pew")
+		shoot()
+	
+
+func shoot():
+	var b = bullet_scene.instantiate()
+	var direction = (get_global_mouse_position() - global_position).normalized()
+	b.setup(direction, $Marker2D.global_position)
+	owner.add_child(b)
